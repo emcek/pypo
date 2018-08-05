594acda26104787ebac246cf99cb19c5a7d43808
Michal Plichta <michal.plichta@nokia.com>
2018-08-06 00:17:59 +0200

    add one test for parse function
    Signed-off-by: Michal Plichta <michal.plichta@nokia.com>

--- tests/test_emerge.py
+++ tests/test_emerge.py
@@ -6,3 +6,20 @@ def test_convert2kibibytes():
     assert convert2kib('23,34 MiB') == 2390016
     assert convert2kib('2 GiB') == 2097152
     assert convert2kib('1,23 GiB') == 128974848
+
+
+def test_parse():
+    from pypo.emerge import parse, EmergeRecord
+    data = '[ebuild   R    ] media-libs/phonon-4.10.1::gentoo  USE="vlc -debug -designer -gstreamer -pulseaudio" 0 KiB\n' \
+           '[ebuild   U    ] x11-libs/phonon-vlc-0.10.1::gentoo  USE="-debug" 0 KiB\n'
+    er = parse(data)
+    assert isinstance(er, list)
+    assert isinstance(er[0], EmergeRecord)
+    assert len(er) == 2
+    assert er[1].action == 'U'
+    assert er[1].category == 'x11-libs'
+    assert er[1].package == 'phonon-vlc'
+    assert er[1].version == '0.10.1'
+    assert isinstance(er[0].flags, dict)
+    assert er[1].flags['USE'] == '-debug'
+    assert er[1].size == 0
