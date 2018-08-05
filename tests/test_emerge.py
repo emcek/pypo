72a5c8fcdbc90d5826dec375b9fe2f49ba404043
Michal Plichta <michal.plichta@nokia.com>
2018-08-05 23:28:49 +0200

    rename convert function
    Signed-off-by: Michal Plichta <michal.plichta@nokia.com>

--- tests/test_emerge.py
+++ tests/test_emerge.py
@@ -1,8 +1,8 @@
 def test_convert2kibibytes():
-    from pypo.emerge import convert2kibibytes
-    assert convert2kibibytes('12 KiB') == 12
-    assert convert2kibibytes('34,567 KiB') == 34567
-    assert convert2kibibytes('31 MiB') == 31744
-    assert convert2kibibytes('23,34 MiB') == 2390016
-    assert convert2kibibytes('2 GiB') == 2097152
-    assert convert2kibibytes('1,23 GiB') == 128974848
+    from pypo.emerge import convert2kib
+    assert convert2kib('12 KiB') == 12
+    assert convert2kib('34,567 KiB') == 34567
+    assert convert2kib('31 MiB') == 31744
+    assert convert2kib('23,34 MiB') == 2390016
+    assert convert2kib('2 GiB') == 2097152
+    assert convert2kib('1,23 GiB') == 128974848
