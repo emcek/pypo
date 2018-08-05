147c3b6472f4e3b948bd20434ae0fb874c5d660a
Michal Plichta <michal.plichta@nokia.com>
2018-08-06 00:53:24 +0200

    setup of logger
    Signed-off-by: Michal Plichta <michal.plichta@nokia.com>

--- pypo/gui.py
+++ pypo/gui.py
@@ -1,5 +1,6 @@
 """Python Portage."""
-from pprint import pprint
+from logging import DEBUG, getLogger, StreamHandler, Formatter, Logger
+from pprint import pformat
 from tkinter import Label, Button, Tk
 
 from pypo.emerge import parse
@@ -14,24 +15,40 @@ class GUI:
 
         :param master: root widget
         """
+        self.log = GUI.set_logger()
+
         self.root = master
         self.root.title('A simple GUI')
 
         self.label = Label(self.root, text='This is our first GUI!')
         self.label.pack()
 
-        self.greet_button = Button(self.root, text='Greet', command=GUI.greet)
+        self.greet_button = Button(self.root, text='Greet', command=self.greet)
         self.greet_button.pack()
 
         self.close_button = Button(self.root, text='Close', command=self.root.quit)
         self.close_button.pack()
 
-    @staticmethod
-    def greet() -> None:
+    def greet(self) -> None:
         """Just show debug data to console."""
         with open('../e01.txt') as emerge_log:
             records_from_file = emerge_log.read()
 
         database = parse(records_from_file)
-        pprint(database)
-        print(len(database))
+        self.log.debug(pformat(database))
+        self.log.info(len(database))
+
+    @staticmethod
+    def set_logger() -> Logger:
+        """
+        Setup of logger.
+
+        :return: instance of configured logger
+        """
+        log = getLogger(__name__)
+        log.setLevel(DEBUG)
+        ch = StreamHandler()
+        ch.setLevel(DEBUG)
+        ch.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        log.addHandler(ch)
+        return log
